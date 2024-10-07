7d89630926294523b74ca063ddbcacb411d9215e
Andres Lemos <Lemos_1325@hotmail.com>
2024-10-06 19:54:51 -0500

    Fix: Transactional annotation is implemented

--- Ms_ClientControl/src/main/java/com/co/cliencontrol/repository/InvoiceRepository.java
+++ Ms_ClientControl/src/main/java/com/co/cliencontrol/repository/InvoiceRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.co.cliencontrol.model.Invoice;
 
+import jakarta.transaction.Transactional;
+
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 
 	Optional<Invoice> findByInvoiceNumber(Long invoiceNumber);
 	
+	@Transactional
 	void deleteByInvoiceNumber(Long invoiceNumber);
 	
 }
