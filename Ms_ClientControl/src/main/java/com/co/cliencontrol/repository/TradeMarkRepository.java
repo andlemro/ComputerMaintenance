1d4d216aa6fde55e3cab1e9ef899eed8af004a6c
Andres Lemos <Lemos_1325@hotmail.com>
2024-10-07 19:31:58 -0500

    fix: the package name is renamed

--- Ms_ClientControl/src/main/java/com/co/cliencontrol/repository/TradeMarkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.cliencontrol.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.co.cliencontrol.model.TradeMark;
-
-public interface TradeMarkRepository extends JpaRepository<TradeMark, Integer> {
-
-}
---
+++ Ms_ClientControl/src/main/java/com/co/clientcontrol/MsClientControlApplication.java
@@ -0,0 +1,13 @@
+package com.co.clientcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientControlApplication.class, args);
+	}
+
+}
