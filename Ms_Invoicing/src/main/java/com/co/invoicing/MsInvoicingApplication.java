e32258d3d19073ca8f41179e3d6cbbca67527e39
Andres Lemos <Lemos_1325@hotmail.com>
2024-10-13 17:06:54 -0500

    feat: ComponentScan implemented to Main classes

--- Ms_Invoicing/src/main/java/com/co/invoicing/MsInvoicingApplication.java
+++ Ms_Invoicing/src/main/java/com/co/invoicing/MsInvoicingApplication.java
@@ -2,8 +2,10 @@ package com.co.invoicing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.co.invoicing.*"})
 public class MsInvoicingApplication {
 
 	public static void main(String[] args) {
