6af597c45e7ec14fbe2b997776f9baf427bc02a1
Andres Lemos <Lemos_1325@hotmail.com>
2025-09-28 15:37:30 -0500

    feat: classes for model test have been created (DeviceTypeTest, MaintenanceKindTest and TradeMarkTest)

---
+++ Ms_ClientControl/src/test/java/com/co/clientcontrol/model/DeviceTypeTest.java
@@ -0,0 +1,55 @@
+package com.co.clientcontrol.model;
+
+import static com.co.clientcontrol.utils.DeviceTypeData.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+@Tag("modelTest")
+public class DeviceTypeTest {
+	
+	private DeviceType deviceType;
+
+	/**************************************************/
+	
+	@BeforeEach
+	void initTest(TestInfo testInfo, TestReporter testReporter) {
+		
+		this.deviceType = DEVICE_TYPE_MODEL_TEST;
+		
+		testReporter.publishEntry(
+				"Running: " + 
+				testInfo.getDisplayName() +
+				" - " +
+				testInfo.getTestMethod().orElse(null).getName() +
+				" with the Tag: " +
+				testInfo.getTags()
+		);
+		
+	}
+	
+	/**************************************************/
+	
+	@Test
+	@DisplayName("Validation of not null in DeviceTypeTest model")
+	void notNullDeviceTypeTest() {
+		assertAll(
+			() -> assertNotNull(this.deviceType.getIdDeviceType(),
+					() -> "The idDeviceType field cannot be null."),
+			() -> assertNotNull(this.deviceType.getDeviceTypeName(), 
+					() -> "The deviceTypeName field cannot be null.")
+		);
+	}
+	
+	/**************************************************/
+	
+	@Test
+	@DisplayName("Validation of maximum length in DeviceTypeTest model")
+	void validateLengthTest() {
+		int deviceTypeNameLimit = 30;
+		assertAll(
+			() -> assertTrue(this.deviceType.getDeviceTypeName().length() < deviceTypeNameLimit,
+					() -> "The deviceTypeName field exceeded the " + deviceTypeNameLimit + " character limit")
+		);
+	}
+}
---
+++ Ms_ClientControl/src/test/java/com/co/clientcontrol/model/TradeMarkTest.java
@@ -0,0 +1,56 @@
+package com.co.clientcontrol.model;
+
+import static com.co.clientcontrol.utils.TradeMarkData.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+@Tag("modelTest")
+public class TradeMarkTest {
+
+	private TradeMark tradeMark;
+	
+	/**************************************************/
+	
+	@BeforeEach
+	void initTest(TestInfo testInfo, TestReporter testReporter) {
+		
+		this.tradeMark = TRADE_MARK_MODEL_TEST;
+		
+		testReporter.publishEntry(
+				"Running: " + 
+				testInfo.getDisplayName() +
+				" - " +
+				testInfo.getTestMethod().orElse(null).getName() +
+				" with the Tag: " +
+				testInfo.getTags()
+		);
+		
+	}
+	
+	/**************************************************/
+	
+	@Test
+	@DisplayName("Validation of not null in TradeMarkTest model")
+	void notNullTradeMarkTest() {
+		assertAll(
+			() -> assertNotNull(this.tradeMark.getIdTradeMark(),
+					() -> "The idTradeMark field cannot be null."),
+			() -> assertNotNull(this.tradeMark.getMarkName(), 
+					() -> "The markName field cannot be null.")
+		);
+	}
+	
+	/**************************************************/
+	
+	@Test
+	@DisplayName("Validation of maximum length in TradeMarkTest model")
+	void validateLengthTest() {
+		int tradeMarkNameLimit = 30;
+		assertAll(
+			() -> assertTrue(this.tradeMark.getMarkName().length() < tradeMarkNameLimit,
+					() -> "The tradeMark field exceeded the " + tradeMarkNameLimit + " character limit")
+		);
+	}
+
+}
